58593e2b95a8d92a027463eddcc49559e9c271af
Pascal Sochacki <pascal@sochacki.dev>
2023-04-16 13:08:51 +0200

    init cdk

--- backstage.go
+++ backstage.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"os"
+
 	"github.com/aws/aws-cdk-go/awscdk/v2"
-	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awseks"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsroute53"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
@@ -18,13 +22,65 @@ func NewBackstageStack(scope constructs.Construct, id string, props *BackstageSt
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	// The code that defines your stack goes here
+	cluster := awseks.NewCluster(stack, jsii.String("Backstage-Cluster"), &awseks.ClusterProps{
+		Version: awseks.KubernetesVersion_V1_25(),
+	})
 
-	// example resource
-	// queue := awssqs.NewQueue(stack, jsii.String("BackstageQueue"), &awssqs.QueueProps{
-	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
-	// })
+	zone := awsroute53.HostedZone_FromLookup(stack, &id, &awsroute53.HostedZoneProviderProps{
+		DomainName: jsii.String("k8s.sochacki.dev"),
+	})
 
+	externalDns := cluster.AddServiceAccount(&id, &awseks.ServiceAccountOptions{
+		Name: jsii.String("external-dns"),
+	})
+
+	externalDns.AddToPrincipalPolicy(
+		awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
+			Actions:   jsii.Strings("route53:ChangeResourceRecordSets", "route53:ListResourceRecordSets"),
+			Resources: jsii.Strings(*zone.HostedZoneArn()),
+		}),
+	)
+
+	externalDns.AddToPrincipalPolicy(
+		awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
+			Actions:   jsii.Strings("route53:ListHostedZones"),
+			Resources: jsii.Strings("*"),
+		}),
+	)
+
+	cluster.AddHelmChart(&id, &awseks.HelmChartOptions{
+		Repository: jsii.String("https://charts.bitnami.com/bitnami"),
+		Chart:      jsii.String("external-dns"),
+		Release:    jsii.String("6.14.3"),
+		Values: &map[string]interface{}{
+			"zoneIdFilters": []string{*zone.HostedZoneId()},
+			"serviceAccount": map[string]interface{}{
+				"create": false,
+				"name":   *externalDns.ServiceAccountName(),
+			},
+		},
+	})
+
+	//const externalDNSHelm = cluster.addHelmChart( 'external-dns', {
+	//repository: 'https://charts.bitnami.com/bitnami',
+	//	chart: 'external-dns',
+	//		release: 'external-dns',
+	//		version: '6.11.3',
+	//		values: {
+	//	zoneIdFilters: [hostedZone.hostedZoneId],
+	//serviceAccount: {
+	//create: false,
+	//name: externalDNS.serviceAccountName,
+	//},
+	//},
+	//});
+	//
+	//cluster.addHelmChart('nginx-ingress', {
+	//repository: 'https://helm.nginx.com/stable',
+	//chart: 'nginx-ingress',
+	//release: 'nginx-ingress',
+	//version: '0.15.1'
+	//})
 	return stack
 }
 
@@ -45,26 +101,9 @@ func main() {
 // env determines the AWS environment (account+region) in which our stack is to
 // be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 func env() *awscdk.Environment {
-	// If unspecified, this stack will be "environment-agnostic".
-	// Account/Region-dependent features and context lookups will not work, but a
-	// single synthesized template can be deployed anywhere.
-	//---------------------------------------------------------------------------
-	return nil
 
-	// Uncomment if you know exactly what account and region you want to deploy
-	// the stack to. This is the recommendation for production stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String("123456789012"),
-	//  Region:  jsii.String("us-east-1"),
-	// }
-
-	// Uncomment to specialize this stack for the AWS Account and Region that are
-	// implied by the current CLI configuration. This is recommended for dev
-	// stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
-	//  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
-	// }
+	return &awscdk.Environment{
+		Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+		Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
+	}
 }
